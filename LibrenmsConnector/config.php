56fcee80dd9fb7ef04fcf8ebac986472f75c9410
Vincenzo Bruno <v.bruno@vinsoft.it>
2022-04-02 20:21:54 +0200

    Corretto bug in config. Aggiunto test della connessione al server LibreNMS

--- LibrenmsConnector/config.php
+++ LibrenmsConnector/config.php
@@ -1,40 +1,39 @@
 <?php
+
 /*
  * @version $Id: HEADER 15930 2011-10-25 10:47:55Z jmd $
- -------------------------------------------------------------------------
- GLPI - Gestionnaire Libre de Parc Informatique
- Copyright (C) 2003-2011 by the INDEPNET Development Team.
+  -------------------------------------------------------------------------
+  GLPI - Gestionnaire Libre de Parc Informatique
+  Copyright (C) 2003-2011 by the INDEPNET Development Team.
 
- http://indepnet.net/   http://glpi-project.org
- -------------------------------------------------------------------------
+  http://indepnet.net/   http://glpi-project.org
+  -------------------------------------------------------------------------
 
- LICENSE
+  LICENSE
 
- This file is part of GLPI.
+  This file is part of GLPI.
 
- GLPI is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
+  GLPI is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
 
- GLPI is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+  GLPI is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with GLPI. If not, see <http://www.gnu.org/licenses/>.
- --------------------------------------------------------------------------
+  You should have received a copy of the GNU General Public License
+  along with GLPI. If not, see <http://www.gnu.org/licenses/>.
+  --------------------------------------------------------------------------
  */
 
 // ----------------------------------------------------------------------
 // Original Author of file:
 // Purpose of file:
 // ----------------------------------------------------------------------
-
 // Non menu entry case
 //header("Location:../../central.php");
-
 // Entry menu case
 const GLPI_ROOT = '../..';
 include (GLPI_ROOT . "/inc/includes.php");
@@ -44,57 +43,55 @@ Session::checkRight("config", UPDATE);
 // To be available when plugin in not activated
 Plugin::load('example');
 
-if($_SESSION["glpiactiveprofile"]["interface"] == "central" || true)
-{
+if ($_SESSION["glpiactiveprofile"]["interface"] == "central") {
     Html::header("Netdiscovery | Config", $_SERVER['PHP_SELF'], "config", "plugins");
-
     $serverip = ApiConfig::getInstance()->getServerIp();
     $apiKey = ApiConfig::getInstance()->getApiKey();
     $token = Session::getNewCSRFToken(true);
+    if (isset($_POST["testconnection"])) {
+        $res = ApiConfig::getInstance()->executeQuery('system');
+        echo '<h3>' . (($res['count'] == 1) ? "Conection OK" : "Connection FAILED") . '</h3><br />';
+    }
 
-    if(!empty($serverip) && !empty($apiKey))
-    {
-        echo "<h3>Current server ip </h3><p>$serverip</p><br>
-            <h3>Current api key </h3><p>$apiKey</p><br>";
-    }
-    else
-    {
-        echo "<h3>No server ip or api key found</h3><br>";
-    }
-    echo "<form method='post' enctype='multipart/form-data' action='config.php'>
-        <p>API key</p>
-        <input type='text' name='api-key' placeholder='API key'><br>
-        <p>Server IP</p>
-        <input type='text' name='server' placeholder='server address'><br>
+    echo "<h3>Configure LibreNMS API Url and Key</h3><br>";
+    echo "<div><form method='post' enctype='multipart/form-data' action='config.php'>
+        <p>API key 
+        <input type='text' name='api-key' placeholder='API key' value='$apiKey'></p><br>
+        <p>Server IP 
+        <input type='text' name='server_ip' placeholder='server address' value='$serverip'></p><br>
         <input type='hidden' name='_glpi_csrf_token' value='$token'>
         <button type='submit' class='save' name='save'><p>Save</p></button>
-    </form>";
+    </form></div>";
+    echo "<div>
+        <form method='post'>
+        <button class='save' name='testconnection' type='submit'><p>Test connection to server</p></button>
+        <input type='hidden' name='_glpi_csrf_token' value='$token'>
+    </form>
+        
+        
+        </div>";
 
-    if(isset($_POST["save"]))
-    {
-        save($_POST["api-key"], $_POST["server"]);
+    if (isset($_POST["save"])) {
+        save($_POST["api-key"], $_POST["server_ip"]);
     }
-}
-else
-{
+} else {
     Html::helpHeader("NetDiscovery", $_SERVER['PHP_SELF']);
     echo "siamo qui";
 }
 
 Html::footer();
 
-function save($apiKey, $serverip)
-{
+function save($apiKey, $serverip) {
     global $DB;
 
     $DB->query("delete from glpi_plugin_netdiscovery");
 
     $DB->insert(
-        "glpi_plugin_netdiscovery",
-        [
-            "api_key" => $apiKey,
-            "server_ip" => $serverip
-        ]
+            "glpi_plugin_netdiscovery",
+            [
+                "api_key" => $apiKey,
+                "server_ip" => $serverip
+            ]
     );
     ApiConfig::getInstance()->restoreData();
     echo "<h1>Configurazione aggiornata</h1>";
--- LibrenmsConnector/inc/apiconfig.class.php
+++ LibrenmsConnector/inc/apiconfig.class.php
@@ -1,19 +1,16 @@
 <?php
 
-class ApiConfig
-{
-    private static $instance = null;
+class ApiConfig {
 
+    private static $instance = null;
     private $serverIp; //= "100.100.201.98/api/v0/";
     private $apiKey; //= "40e1d3572181097d240f08b7b2cff9b0";
 
-    private function __construct()
-    {
+    private function __construct() {
         $this->fetchData();
     }
 
-    private function fetchData()
-    {
+    private function fetchData() {
         global $DB;
         $result = $DB->request([
             "FROM" => "glpi_plugin_netdiscovery",
@@ -21,42 +18,36 @@ class ApiConfig
             "LIMIT" => 1
         ]);
 
-        foreach ($result as $row)
-        {
+        foreach ($result as $row) {
             $this->apiKey = $row["api_key"];
             $this->serverIp = $row["server_ip"];
             break;
         }
     }
 
-    public static function getInstance(): ApiConfig
-    {
-        if(self::$instance == null)
-        {
+    public static function getInstance(): ApiConfig {
+        if (self::$instance == null) {
             self::$instance = new ApiConfig();
         }
         return self::$instance;
     }
 
-    public function executeQuery($route)
-    {
-        $url = "curl -H 'X-Auth-Token: " . $this->apiKey ."' " . $this->serverIp . "/api/v0/" . "$route";
+    public function executeQuery($route) {
+        $url = "curl -H 'X-Auth-Token: " . $this->apiKey . "' " . $this->serverIp . "/api/v0/" . "$route";
         $json = shell_exec($url . " > output.json");
         return json_decode(file_get_contents("output.json"), true);
     }
 
-    public function restoreData(): void
-    {
+    public function restoreData(): void {
         $this->fetchData();
     }
 
-    public function getApiKey(): string
-    {
-        return ($this->apiKey) ? $this->getApiKey() : "";
+    public function getApiKey(): string {
+        return ($this->apiKey) ? $this->apiKey : "";
     }
 
-    public function getServerIp(): string
-    {
-        return ($this->serverIp) ? $this->getServerIp() : "";
+    public function getServerIp(): string {
+        return ($this->serverIp) ? $this->serverIp : "";
     }
+
 }
